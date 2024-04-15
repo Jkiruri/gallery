c3fe1336cb7900aa01067504a39dd29db7b4753e
James <jamesnjunge45@gmail.com>
2024-04-15 10:49:18 +0300

    upload

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'ongodb+srv://admin:OZmjRCGeGNSymPVj@cluster0.eaqif8y.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0&ssl=true',
+    production: 'mongodb+srv://admin:OZmjRCGeGNSymPVj@cluster0.eaqif8y.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0&ssl=true',
     development: 'mongodb+srv://admin:OZmjRCGeGNSymPVj@cluster0.eaqif8y.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0&ssl=true',
     test: 'mongodb+srv://admin:OZmjRCGeGNSymPVj@cluster0.eaqif8y.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0&ssl=true',
 
